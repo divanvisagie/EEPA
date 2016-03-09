4f49ec21786d3ad91a453aaca71ad4d33a105a0a
Steven van der Merwe <steven.vandermerwe@kaleidocode.co.za>
2016-03-09 11:00:52 +0200

    Initial Commit

---
+++ EEPA.Api/Modules/IndexModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace EEPA.Api.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule() : base("/api")
+        {
+            Get["/"] = Index;
+        }
+
+        private dynamic Index(dynamic o)
+        {
+            return "Api is running";
+        }
+    }
+}
---
+++ EEPA.Test/TestMessageQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using RabbitMQ.Client;
+
+namespace EEPA.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestMessageQueue
+    {
+        private const string QueueName = "hello2";
+
+        [Test]
+        public void TestMessageQueueIsRunning()
+        {
+            //---------------Set up test pack-------------------
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            
+            var message = "Hello World!";
+            var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", QueueName, null, encodedMessage);
+            Console.WriteLine(" [x] Sent '" + message + "'");
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            channel.Close();
+            connection.Close();
+            
+        }
+    }
+}
